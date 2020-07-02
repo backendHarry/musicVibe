afb08c7dc74422c4cc698a0b3ecfc22c1d9ffdd2
Muskan Bartaula <30722278+MuskanBartaula@users.noreply.github.com>
2020-07-02 10:54:49 +0545

    Update forms.py

--- AllMedia/forms.py
+++ AllMedia/forms.py
@@ -9,10 +9,10 @@ class MediaFilesForm(forms.ModelForm):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		self.fields['files'].widget.attrs.update({
-                    'class':'hide-me',
-                    'id':'fileElem',
-                    'style':'display:none;',
-                    'multiple':True,
+                    'class': 'hide-me',
+                    'id': 'fileElem',
+                    'style': 'display:none;',
+                    'multiple': True,
                 })
 
 #forms for saving media files from user
