6364ec591ad32f076c6beafd2aa7f7ccc223d3fc
Muskan Bartaula <30722278+MuskanBartaula@users.noreply.github.com>
2020-07-02 10:59:01 +0545

    Update models.py

--- AllMedia/models.py
+++ AllMedia/models.py
@@ -7,12 +7,14 @@ class MediaFiles(models.Model):
     user_for_files = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=128)
     files = models.FileField(upload_to='media')
+    
+    class Meta:
+        verbose_name = 'MediaFile'
+        verbose_name_plural = 'MediaFiles'
 
     def __str__(self):
         return self.name
 
-    class Meta:
-        verbose_name = 'MediaFile'
-        verbose_name_plural = 'MediaFiles'
+    
 
 #my model for my user files uploaded by users
