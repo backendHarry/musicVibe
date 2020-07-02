d663d360332e5b52880e36d0e6c72cdb49987f7c
Muskan Bartaula <30722278+MuskanBartaula@users.noreply.github.com>
2020-07-02 10:45:06 +0545

    Update models.py
    Use settings.AUTH_USER_MODEL when using it as a foreign key.

--- AllMedia/models.py
+++ AllMedia/models.py
@@ -1,19 +1,18 @@
 from django.db import models
-from django.contrib.auth import get_user_model
+from django.conf import settings
 
 # Create your models here.
-User = get_user_model()
-
+User = settings.AUTH_USER_MODEL
 class MediaFiles(models.Model):
-	user_for_files = models.ForeignKey(User, on_delete=models.CASCADE)
-	name = models.CharField(max_length=128)
-	files = models.FileField(upload_to='media')
+    user_for_files = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128)
+    files = models.FileField(upload_to='media')
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
-	class Meta:
-		verbose_name = 'MediaFile'
-		verbose_name_plural = 'MediaFiles'
+    class Meta:
+        verbose_name = 'MediaFile'
+        verbose_name_plural = 'MediaFiles'
 
 #my model for my user files uploaded by users
